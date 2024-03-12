15b8e94c9dbf12a90cbaf5711433b9c6227ae1b8
Shilan1mabuza <163201798+Shilan1mabuza@users.noreply.github.com>
2024-03-12 22:44:52 +0200

    Add files via upload

---
+++ home.php
@@ -0,0 +1,49 @@
+
+<style>
+   
+</style>
+
+<div class="containe-fluid">
+
+	<div class="row">
+		<div class="col-lg-12">
+                    <div class="row mt-3 ml-3 mr-3">
+			<div class="col-lg-12">
+                <div class="card">
+                    <div class="card-body">
+                    <?php echo "Welcome back ". $_SESSION['login_name']."!"  ?>
+                                        
+                    </div>
+                    
+		</div>
+                            <br>
+            <div class="row">
+		<div class="col-lg-12">
+			<div class="card col-md-4 offset-2 bg-info float-left">
+				<div class="card-body text-white">
+					<h4><b>Assigned Contracts</b></h4>
+					<hr>
+					<span class="card-icon"><i class="fa fa-users"></i></span>
+					<h3 class="text-right"><b></b></h3>
+				</div>
+                            
+			</div>
+			<div class="card col-md-4 offset-2 bg-primary ml-4 float-left">
+				<div class="card-body text-white">
+					<h4><b>Completed Contracts</b></h4>
+					<hr>
+					<span class="card-icon"><i class="fa fa-user-tie"></i></span>
+					<h3 class="text-right"><b></b></h3>
+				</div>
+	</div>
+
+	
+                    
+                </div>
+            </div>
+	</div>
+
+</div>
+<script>
+	
+</script>
